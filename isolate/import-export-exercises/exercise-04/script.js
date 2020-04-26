cfe3b391ceaa2da2cdbfb125a26fbff358cea3ff
ekaterinaasf <59531760+ekaterinaasf@users.noreply.github.com>
2020-04-26 13:13:06 +0200

    Update script.js

--- isolate/import-export-exercises/exercise-04/script.js
+++ isolate/import-export-exercises/exercise-04/script.js
@@ -1,4 +1,4 @@
-let months = ['Jan', 'Feb', 'Mar', 'Apr', 'Aug', _, 'Oct', 'Nov', 'Dec'];
+let months = ['Jan', 'Feb', 'Mar', 'Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 const MODULES_BECAME_STANDARD_YEAR = 1472;
 
@@ -7,3 +7,5 @@ class User {
     this.name = name;
   }
 }
+
+export {months, MODULES_BECAME_STANDARD_YEAR, User};
